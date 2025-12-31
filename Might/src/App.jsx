26aaa1785a707101a2643d411630c09c4f01b29b
prahladgaur7 <prahladgaur0711@gmail.com>
2025-12-31 14:35:04 +0530

    code

---
+++ Might/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='w-full h-screen bg-red-200 items-center justify-center'>
+      <h1 className='text-center'>This is Might App</h1>
+      <h1 className="text-center">This good</h1>
+    
+    </div></>
+  )
+}
+
+export default App
